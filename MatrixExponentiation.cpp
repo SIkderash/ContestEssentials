e5876364dd66e9f7835533819c63234109e4d7c2
SIkderash <48786809+SIkderash@users.noreply.github.com>
2021-04-22 11:41:41 +0600

    Update and rename conseprime.cpp to MatrixExponentiation.cpp

---
+++ MatrixExponentiation.cpp
@@ -0,0 +1,25 @@
+   void multiply(ull a[][2], ull b[][2]){
+        ull temp[2][2] = {{0,0},{0,0}};
+        for(int i=0; i<2; i++){
+            for(int j=0; j<2; j++){
+                for(int k=0; k<2; k++){
+                    temp[i][j] += a[i][k]*b[k][j];
+                }
+            }
+        }
+        for(int i=0; i<2; i++){
+            for(int j=0; j<2; j++){
+                a[i][j] = temp[i][j];
+            }
+        }
+    }
+    void matrixExpo(ull a[][2], ull power){
+        if(power==0 || power==1) return;
+        ull temp[2][2] = {{0,z},{1,z}};
+        matrixExpo(a,power/2);
+        multiply(a,a);
+        if(power&1) multiply(a,temp);
+    }
+    /*
+
+    */
